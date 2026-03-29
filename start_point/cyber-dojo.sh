c8a6c518e4bdaac5d613879001b5a5d5aba1fae7
JonJagger <jon@kosli.com>
2026-03-29 15:17:39 +0100

    Upgrade to 1.8.1

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# There is no internet access so do not contact any module proxy
+# to download or resolve modules.
+export GOPROXY=off
+# There is no internet access so do not verify any module's checksum
+# against the public checksum database (sum.golang.org)
+export GONOSUMDB='*'
+# Use cache pre-populated in the docker image we are running in.
+export GOCACHE=/go/build-cache
+
 go test
